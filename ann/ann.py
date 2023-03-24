ff0485c0bbec22f7a9dec4e1b82f255b2e227ffa
Noah Weiner <noah.weiner@yale.edu>
2023-03-24 01:18:10 -0400

    progress on annotation system using PySide, also create zoomable pannable map on Android canvas

--- ann/ann.py
+++ ann/ann.py
@@ -1,45 +1,207 @@
 import cv2
+from PySide6.QtWidgets import (
+    QApplication,
+    QFrame,
+    QListWidget,
+    QListWidgetItem,
+    QPushButton,
+    QMainWindow,
+    QGridLayout,
+    QLineEdit,
+    QLabel,
+    QErrorMessage,
+    QComboBox
+)
+from dialogs import FixedWidthMessageDialog, FW_FONT
+import sys
+import os
+
+VIDS_DIR = "/home/nodog/docs/files/YaleSenior/cs490/cs490/data_prep/vids"
+
+def absoluteFilePaths(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            #yield suspends function’s execution and sends value back to caller, but retains state to enable fxn to resume where left off. When the function resumes, it continues execution immediately after the last yield run. 
+            #This allows it to produce series of values over time, rather than computing them at once and sending them back like a list.
+            yield os.path.abspath(os.path.join(dirpath, f))
 
 
-#open the video using VideoCapture obj
-cap = cv2.VideoCapture('../vids/aisle_left.mp4')
+class AnnotatorGUI:
+  def __init__(self):
+    """Overhead management of GUI"""
+    self.vid = None
 
-if (cap.isOpened() == False): 
-  print("Error opening video stream or file")
- 
-#read frames until video is done
-while(cap.isOpened()):
-    #read a frame
-    ret, frame = cap.read()
+    self.app = QApplication()
 
-    #make sure frame read successfully
-    if ret == True:
-        #create the cv2 window and give it name
-        cv2.namedWindow("aisle", cv2.WINDOW_NORMAL)
+    # Highest level layout: input & output
+    page_layout = QGridLayout()
 
-        #resize window to custom size
-        cv2.resizeWindow("aisle", 500, 900)
+    # Manager for user unput interaction: entries and submit
+    user_layout = QGridLayout()
 
-        #display a frame in the window
-        cv2.imshow("aisle", frame)
+    # Manager for entries: label & line (included in user_layout)
+    entry_layout = QGridLayout()
 
-        #can press q to quit
-        #if cv2.waitKey(25) & 0xFF == ord('q'):
-        #    break
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
+    #Delegate function for simple reg searching
+    def search_call_back():
+      return None
+
+    # Add four fields to the application window for searching
+    dept_layout = AnnotatorGUI._create_entry('Search for product:')
+    entry_layout.addLayout(dept_layout, 0, 0)
+
+    # QPushButton that helps reg.py communicate with regserver.py
+    vid_button = QPushButton('Play Video')
+    vid_button.clicked.connect(self.play_vid)
+
+    search_button = QPushButton('Search')
+    search_button.clicked.connect(search_call_back)
+
+    vids_dropdown = QComboBox()
+    vids_dropdown.addItems(absoluteFilePaths(VIDS_DIR))
+    vids_dropdown.currentTextChanged.connect(self.vid_text_changed)
+
+    user_layout.addWidget(vids_dropdown, 1, 0)
+
+    # Merging top level user input layouts
+    user_layout.addLayout(entry_layout, 0, 0)
+    user_layout.addWidget(vid_button, 2, 0)
+    user_layout.addWidget(search_button, 1, 1)
+
+    # Output layout, list automatically horizontally/vertically scrolls
+    self.output_widget = QListWidget()
+    self.output_widget.setFont(FW_FONT)
+
+    # Event: double clicked or enter (or Cmd+O for Mac)
+    #self.output_widget.itemActivated.connect(details_call_back)
+
+    page_layout.addLayout(user_layout, 0, 0)
+    page_layout.addWidget(self.output_widget, 1, 0)
+
+    self.frame = QFrame()
+    self.frame.setLayout(page_layout)
+
+    self._create_window()
+
+
+
+  def vid_text_changed(self, text):
+    self.vid = text
+
+
+  #A static method doesn't receive any reference argument whether it is called by an instance of a class or by the class itself
+  @staticmethod
+  def _create_entry(name:str) -> QGridLayout:
+      """Creates a label and lineedit for {name} with callbacks
+      update_cb: method for storing the line edit's text
+      query_cb: method for submitting the query request
+      """
+
+      label = QLabel(name)
+      entry = QLineEdit()
+      
+      '''
+      entry.textChanged.connect(
+          lambda text: update_cb(text)
+      )
+      entry.returnPressed.connect(query_cb)'''
+      layout = QGridLayout()
+      layout.addWidget(label, 0, 0)
+      layout.addWidget(entry, 0, 1)
+
+      return layout
+
+
+  def _create_window(self):
+        """Develops the default window"""
+
+        # Create window and add frame
+        self.window = QMainWindow()
+        self.window.setWindowTitle('Annotation System')
+        self.window.setCentralWidget(self.frame)
+
+        # Ensures dialog size will be <= 25% of window
+        screen_size = self.app.primaryScreen().availableGeometry()
+        self.window.resize(screen_size.width() // 2, screen_size.height() // 2)
+
+  def run(self):
+        """Creates GUI and responses to events"""
+
+        self.window.show()
+        sys.exit(self.app.exec())
+
+
+  def play_vid(self):
+    #in inches
+    aisle_dist = 965
+    aisle_width = 107
+
+    #open the video using VideoCapture obj
+    cap = cv2.VideoCapture(self.vid)
+
+    if (cap.isOpened() == False): 
+      print("ERROR opening video stream or file")
+      exit()
+
+    #get video fps
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(f"{fps} frames per second")
+
+    #get num of frames
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    if (fps == 0.0):
+      print("ERROR: get returned 0.0 frames per second")
+      exit()
+
+    print("frame_count is", frame_count)
+    duration = frame_count / fps
+    print("Duration of video is", duration, "seconds")
+
+    framectr = 0
+    
+    #read frames until video is done
+    while(cap.isOpened()):
+        #read a frame
+        ret, frame = cap.read()
+
+        #make sure frame read successfully
+        if ret == True:
+            #create the cv2 window and give it name
+            cv2.namedWindow("aisle", cv2.WINDOW_NORMAL)
+
+            #resize window to custom size
+            cv2.resizeWindow("aisle", 500, 900)
+
+            #display a frame in the window
+            cv2.imshow("aisle", frame)
+
+            #can press q to quit
+            #if cv2.waitKey(25) & 0xFF == ord('q'):
+            #    break
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            if key == ord('p') or key == ord(' '):
+                print("Paused at frame number", framectr, "and at time", framectr/fps)
+                cv2.waitKey(-1) #wait until any key is pressed
+
+            framectr += 1
+
+        #something wrong opening frame, so break out of loop now
+        else: 
             break
-        if key == ord('p'):
-            cv2.waitKey(-1) #wait until any key is pressed
+    
 
-    #something wrong opening frame, so break out of loop now
-    else: 
-        break
- 
+    #release video capture obj
+    cap.release()
+    
+    #close all cv2 windows
+    cv2.destroyAllWindows()
 
-#release video capture obj
-cap.release()
- 
-#close all cv2 windows
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    gui = AnnotatorGUI()
+    gui.run()
--- app/app/src/main/java/weiner/noah/groceryguide/StoreMap2D.java
+++ app/app/src/main/java/weiner/noah/groceryguide/StoreMap2D.java
@@ -1,18 +1,28 @@
 package weiner.noah.groceryguide;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.view.MotionEventCompat;
+import androidx.core.view.ViewCompat;
+
+import java.util.ArrayList;
 
 public class StoreMap2D extends View {
 
@@ -29,11 +39,29 @@ public class StoreMap2D extends View {
     private float mScaleFactor = 1.f;
 
     private final int INVALID_POINTER_ID = -1;
+    private final int AXIS_X_MIN = 0;
+    private final int AXIS_X_MAX = 1000;
+    private final int AXIS_Y_MIN = 0;
+    private final int AXIS_Y_MAX = 2000;
+
+    private float mLastGestureX;
+    private float mLastGestureY;
+
     private int mActivePointerId = INVALID_POINTER_ID;
 
     private float mLastTouchX, mLastTouchY;
     private float mPosX, mPosY;
 
+    private int left = 0;
+    private int top = 200;
+    private int right = 1080;
+    private int bottom = 1100;
+
+    private final String TAG = "StoreMap2D";
+
+    private Drawable d;
+
+    private SSWhalley ssWhalley;
 
     public StoreMap2D(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -52,7 +80,12 @@ public class StoreMap2D extends View {
             a.recycle();
         }
 
-        mScaleDetector = new ScaleGestureDetector(context, new ScaleGestureDetector.SimpleOnScaleGestureListener());
+        ssWhalley = new SSWhalley();
+
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        d = ResourcesCompat.getDrawable(getResources(), R.drawable.svg_floor, null);
+        assert d != null;
+        d.setBounds(left, top, right, bottom);
 
         //init the Paint
         init();
@@ -96,49 +129,82 @@ public class StoreMap2D extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // Draw the shadow
-        //canvas.drawOval(shadowBounds, shadowPaint);
+        //dims are 2200x1375
+        //dims are actually 1999x1080
+        int ht = getHeight();
+        int width = getWidth();
+        //Log.i(TAG, "Canvas dims are height " + ht + " and width " + width);
 
+        canvas.save();
+        canvas.translate(mPosX, mPosY);
+        Log.i(TAG, "mScaleFactor is " + mScaleFactor);
+        canvas.scale(mScaleFactor, mScaleFactor);
+
+        /*
         textPaint.setTextSize(100);
         // Draw the label text
         canvas.drawText("TEST", 100, 100, textPaint);
 
-        /*
-        // Draw the pie slices
-        for (int i = 0; i < data.size(); ++i) {
-            ClipData.Item it = data.get(i);
-            piePaint.setShader(it.shader);
-            canvas.drawArc(bounds, 360 - it.endAngle, it.endAngle - it.startAngle, true, piePaint);
-        }*/
-
         textPaint.setStrokeWidth(10);
+
         //draw the pointer
         canvas.drawLine(200, 200, 400, 400, textPaint);
-        canvas.drawCircle(700, 700, 100, textPaint);
+        canvas.drawCircle(600, 600, 100, textPaint);
+        */
+
+        //d.draw(canvas);
+
+        ArrayList<SSWhalley.StoreElement> elements = ssWhalley.getRectList();
+
+        for (int i = 0; i < elements.size(); i++) {
+            if (i == 0) {
+                textPaint.setColor(Color.parseColor("#f6def7"));
+            } else {
+                textPaint.setColor(Color.parseColor("#cccccc"));
+            }
+
+            RectF thisRect = elements.get(i).getRect();
+
+            //save canvas state before possible rotation
+            canvas.save();
+            float rot = elements.get(i).getRot();
+            if (rot != 0f) {
+                canvas.rotate(rot, thisRect.centerX(), thisRect.centerY());
+            }
+
+            canvas.drawRect(thisRect, textPaint);
+            canvas.restore();
+        }
+
+        //canvas.drawRect(SSWhalley.aisle1, textPaint);
+
+        //return canvas to state it was in upon entering onDraw()
+        canvas.restore();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         // Account for padding
-        float xpad = (float)(getPaddingLeft() + getPaddingRight());
-        float ypad = (float)(getPaddingTop() + getPaddingBottom());
+        float xpad = (float) (getPaddingLeft() + getPaddingRight());
+        float ypad = (float) (getPaddingTop() + getPaddingBottom());
 
         // Account for the label
         if (mShowText) xpad += textWidth;
 
-        float ww = (float)w - xpad;
-        float hh = (float)h - ypad;
+        float ww = (float) w - xpad;
+        float hh = (float) h - ypad;
 
         // Figure out how big we can make the pie.
         float diameter = Math.min(ww, hh);
 
-        super.onSizeChanged((int)ww, (int)hh, oldw, oldh);
+        super.onSizeChanged((int) ww, (int) hh, oldw, oldh);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         // Let the ScaleGestureDetector inspect all events.
         mScaleDetector.onTouchEvent(ev);
+
 
         final int action = MotionEventCompat.getActionMasked(ev);
 
@@ -178,21 +244,6 @@ public class StoreMap2D extends View {
                 mLastTouchX = x;
                 mLastTouchY = y;
 
-                /*
-                animate()
-                        .x(mPosX)
-                        .y(mPosY)
-                        .setDuration(0)
-                        .start();*/
-
-                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) getLayoutParams();
-                layoutParams.leftMargin = (int)mPosX;
-                layoutParams.topMargin = (int)mPosY;
-                layoutParams.rightMargin = -250;
-                layoutParams.bottomMargin = -250;
-                setLayoutParams(layoutParams);
-                invalidate();
-
                 break;
             }
 
@@ -218,5 +269,65 @@ public class StoreMap2D extends View {
             }
         }
         return true;
+    }
+
+    /**
+     * Sets the current viewport (defined by mCurrentViewport) to the given
+     * X and Y positions. Note that the Y value represents the topmost pixel position,
+     * and thus the bottom of the mCurrentViewport rectangle.
+     */
+    private void setViewportBottomLeft(float x, float y) {
+        /*
+         * Constrains within the scroll range. The scroll range is simply the viewport
+         * extremes (AXIS_X_MAX, etc.) minus the viewport size. For example, if the
+         * extremes were 0 and 10, and the viewport size was 2, the scroll range would
+         * be 0 to 8.
+         */
+
+        float curWidth = mCurrentViewport.width();
+        float curHeight = mCurrentViewport.height();
+        x = Math.max(AXIS_X_MIN, Math.min(x, AXIS_X_MAX - curWidth));
+        y = Math.max(AXIS_Y_MIN + curHeight, Math.min(y, AXIS_Y_MAX));
+
+        mCurrentViewport.set(x, y - curHeight, x + curWidth, y);
+
+        //Invalidates the View to update the display.
+        ViewCompat.postInvalidateOnAnimation(this);
+    }
+
+    //RectF is a float rectangle
+    // The current viewport. This rectangle represents the currently visible chart domain and range.
+    private RectF mCurrentViewport = new RectF(AXIS_X_MIN, AXIS_Y_MIN, AXIS_X_MAX, AXIS_Y_MAX);
+
+    //The current destination rectangle (in pixel coordinates) into which the chart data should be drawn.
+    private Rect mContentRect;
+
+    private final GestureDetector.SimpleOnGestureListener mGestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            // Scrolling uses math based on the viewport (as opposed to math using pixels).
+
+            // Pixel offset is the offset in screen pixels, while viewport offset is the offset within the current viewport.
+            float viewportOffsetX = distanceX * mCurrentViewport.width() / mContentRect.width();
+            float viewportOffsetY = -distanceY * mCurrentViewport.height() / mContentRect.height();
+
+            // Updates the viewport, refreshes the display.
+            setViewportBottomLeft(mCurrentViewport.left + viewportOffsetX, mCurrentViewport.bottom + viewportOffsetY);
+
+            return true;
+        }
+    };
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+
+            invalidate();
+            return true;
+        }
     }
 }
